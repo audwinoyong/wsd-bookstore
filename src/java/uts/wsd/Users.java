8ffc8a6c2eada0ed576849b91508d5f45a4f59b2
Audwin Oyong <11814771@student.uts.edu.au>
2018-05-08 18:41:48 +1000

    Fix DiaryService and Users class

--- src/java/uts/wsd/Users.java
+++ src/java/uts/wsd/Users.java
@@ -1,19 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package uts.wsd;
  
 import java.util.*;
 import java.io.Serializable;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
- 
+import javax.xml.bind.annotation.*;
+
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "users")
 public class Users implements Serializable {
     @XmlElement(name = "user")
     private ArrayList<User> list = new ArrayList<User>();
  
-
     public ArrayList<User> getList() {
         return list;
     }
@@ -30,5 +31,15 @@ public class Users implements Serializable {
                 return user; // Login correct. Return this user.
         }
         return null; // Login incorrect. Return null.
+    }
+    
+    // function to check if email is already exists
+    public User getUser(String email) {
+        for (User user : list) {
+            if (user.getEmail().equals(email)) {
+                return user; //the email is not registered yet.
+            }
+        }
+        return null;
     }
 }
--- src/java/uts/wsd/rest/DiaryService.java
+++ src/java/uts/wsd/rest/DiaryService.java
@@ -1,87 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package uts.wsd.rest;
- 
+
 import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import javax.xml.bind.JAXBException;
 import java.io.*;
-import uts.wsd.DiaryApplication;
-import uts.wsd.User;
-import uts.wsd.Users;
- 
+import uts.wsd.*;
+
 @Path("/diaryApp")
 public class DiaryService {
- @Context
- private ServletContext application;
- 
- private DiaryApplication getDiaryApp() throws JAXBException, IOException {
-  // The web server can handle requests from different clients in parallel.
-  // These are called "threads".
-  //
-  // We do NOT want other threads to manipulate the application object at the same
-  // time that we are manipulating it, otherwise bad things could happen.
-  //
-  // The "synchronized" keyword is used to lock the application object while
-  // we're manpulating it.
-  synchronized (application) {
-   DiaryApplication diaryApp = (DiaryApplication)application.getAttribute("diaryApp");
-   if (diaryApp == null) {
-    diaryApp = new DiaryApplication();
-    diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
-    application.setAttribute("diaryApp", diaryApp);
-   }
-   return diaryApp;
-  }
- }
+
+    @Context
+    private ServletContext application;
+
+    private DiaryApplication getDiaryApp() throws JAXBException, IOException {
+        // The web server can handle requests from different clients in parallel.
+        // These are called "threads".
+        //
+        // We do NOT want other threads to manipulate the application object at the same
+        // time that we are manipulating it, otherwise bad things could happen.
+        //
+        // The "synchronized" keyword is used to lock the application object while
+        // we're manpulating it.
+        synchronized (application) {
+            DiaryApplication diaryApp = (DiaryApplication) application.getAttribute("diaryApp");
+            if (diaryApp == null) {
+                diaryApp = new DiaryApplication();
+                diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
+                application.setAttribute("diaryApp", diaryApp);
+            }
+            return diaryApp;
+        }
+    }
+
+    /**
+     * YOUR METHODS WILL BE INSERTED HERE *
+     */
+    @GET
     @Path("users")
-    @GET
     @Produces(MediaType.APPLICATION_XML)
-    public Users getUsers ()  throws JAXBException, IOException {
-        synchronized (application) {
-          DiaryApplication diaryApp = (DiaryApplication)application.getAttribute("diaryApp");
-          if (diaryApp == null) {
-           diaryApp = new DiaryApplication();
-           diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
-           application.setAttribute("diaryApp", diaryApp);
-          }
-         return diaryApp.getUsers();
-        }
+    public Users getUsers() throws JAXBException, IOException {
+        return getDiaryApp().getUsers();
     }
-    
+
+    @GET
     @Path("users/{email}")
-    @GET
     @Produces(MediaType.APPLICATION_XML)
-    public User getUser (@PathParam("email") String email)  throws JAXBException, IOException {
-        synchronized (application) {
-          DiaryApplication diaryApp = (DiaryApplication)application.getAttribute("diaryApp");
-          if (diaryApp == null) {
-           diaryApp = new DiaryApplication();
-           diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
-           application.setAttribute("diaryApp", diaryApp);
-          }
-          for (User user : diaryApp.getUsers().getList())
-          if (user.getEmail().equals(email)) 
-              return user;
-          return null;
-        }
+    public User getUser(@PathParam("email") String email) throws JAXBException, IOException {
+        return getDiaryApp().getUsers().getUser(email);
     }
-    
-    @Path("users")    
+
     @POST
+    @Path("users")
     @Consumes(MediaType.APPLICATION_XML)
-    public void addUser (User user) throws JAXBException, IOException {
-        synchronized (application) {
-          DiaryApplication diaryApp = (DiaryApplication)application.getAttribute("diaryApp");
-          if (diaryApp == null) {
-           diaryApp = new DiaryApplication();
-           diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
-           application.setAttribute("diaryApp", diaryApp);
-          }
-          diaryApp.getUsers().addUser(user);
-          diaryApp.saveUsers();
-        }
-    }    
-        
- 
- /** YOUR METHODS WILL BE INSERTED HERE **/
+    public void addUser(User user) throws JAXBException, IOException {
+        getDiaryApp().getUsers().addUser(user);
+        getDiaryApp().saveUsers();
+    }
 }
