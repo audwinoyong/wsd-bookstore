c81cd8d3ebc1d50b1760246ea536d2fa875a6987
audwinoyong <audwinoyong@live.com>
2018-05-24 03:04:55 +1000

    Fix bug in adding book for SOAP Client

--- src/java/uts/wsd/soap/BookSOAP.java
+++ src/java/uts/wsd/soap/BookSOAP.java
@@ -25,118 +25,114 @@ import uts.wsd.User;
  */
 @WebService(serviceName = "bookApp")
 public class BookSOAP {
-    //fetch all ressources 
+
+    //fetch all ressources
     @Resource
     private WebServiceContext context;
-   //book handler 
+    //book handler
+
     private BookApplication getBookApp() {
         try {
-        ServletContext application = (ServletContext)context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
-        synchronized (application) {
-            BookApplication bookApp = (BookApplication) application.getAttribute("bookApp");
-            if (bookApp == null) {
-                bookApp = new BookApplication();
-                bookApp.setFilePath(application.getRealPath("WEB-INF/books.xml"));
-                application.setAttribute("bookApp", bookApp);
-            }
-            return bookApp;
-        }
-        }
-        catch (Exception e) {
-        System.out.println("error");
-        return null;
-        }
-    }
-    //user handler
-    public DiaryApplication getDiaryApp() {
-            // This needs to be local variable in your "getDiaryApp" method
-            // It will not work if declared as a field (since field initializers
-            // are executed before dependency injection takes place).
-            try {
-            ServletContext application = (ServletContext)context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
-              synchronized (application) {
-                DiaryApplication diaryApp = (DiaryApplication)application.getAttribute("diaryApp");
-                if (diaryApp == null) {
-                 diaryApp = new DiaryApplication();
-                 diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
-                 application.setAttribute("diaryApp", diaryApp);
+            ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+            synchronized (application) {
+                BookApplication bookApp = (BookApplication) application.getAttribute("bookApp");
+                if (bookApp == null) {
+                    bookApp = new BookApplication();
+                    bookApp.setFilePath(application.getRealPath("WEB-INF/books.xml"));
+                    application.setAttribute("bookApp", bookApp);
                 }
-                return diaryApp;
-              }
+                return bookApp;
             }
-            catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("error");
             return null;
-            }
         }
-    
+    }
+
+    //user handler
+    public DiaryApplication getDiaryApp() {
+        // This needs to be local variable in your "getDiaryApp" method
+        // It will not work if declared as a field (since field initializers
+        // are executed before dependency injection takes place).
+        try {
+            ServletContext application = (ServletContext) context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+            synchronized (application) {
+                DiaryApplication diaryApp = (DiaryApplication) application.getAttribute("diaryApp");
+                if (diaryApp == null) {
+                    diaryApp = new DiaryApplication();
+                    diaryApp.setFilePath(application.getRealPath("WEB-INF/users.xml"));
+                    application.setAttribute("diaryApp", diaryApp);
+                }
+                return diaryApp;
+            }
+        } catch (Exception e) {
+            System.out.println("error");
+            return null;
+        }
+    }
+
     @WebMethod()
     public User userLogin(String email, String password) {
         try {
-        ArrayList<User> users = getDiaryApp().getUsers().getList();
+            ArrayList<User> users = getDiaryApp().getUsers().getList();
 
-        for (User user : users) {
-            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
-                return user; 
+            for (User user : users) {
+                if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                    return user;
+                }
             }
-        }
-        return null; 
-        }
-            catch (Exception e) {
+            return null;
+        } catch (Exception e) {
             System.out.println("error");
             return null;
-            }
+        }
     }
-    
-    @WebMethod()    
+
+    @WebMethod()
     public Books fetchBooks() {
         try {
             return getBookApp().getBooks();
-        }
-        catch (Exception e) {
-        System.out.println("error");
-        return null;
+        } catch (Exception e) {
+            System.out.println("error");
+            return null;
         }
     }
-    
+
     @WebMethod()
     public ArrayList<Book> getBooksByUser(String username) {
         try {
-        ArrayList<Book> booklist = getBookApp().getBooks().getBooksByUsername(username);
-        return booklist;
-        }
-        catch (Exception e) {
-        System.out.println("error");
-        return null;
+            ArrayList<Book> booklist = getBookApp().getBooks().getBooksByUsername(username);
+            return booklist;
+        } catch (Exception e) {
+            System.out.println("error");
+            return null;
         }
     }
+
     @WebMethod()
     public void addBook(String booktitle, String author, String category, String condition, String isbn, int publishYear, String publisher, String username, String abst) {
         try {
-        Book book = new Book(booktitle, author, category, condition, isbn, publishYear, publisher, username, abst);
-        Books books = getBookApp().getBooks();
-        books.addBook(book);
-        getBookApp().updateXML(books, "WEB-INF/books.xml");
-        getBookApp().saveBooks();
+            Book book = new Book(booktitle, author, category, condition, isbn, publishYear, publisher, username, abst);
+            Books books = getBookApp().getBooks();
+            books.addBook(book);
+            getBookApp().updateXML(books, "WEB-INF/books.xml");
+            getBookApp().saveBooks();
+        } catch (Exception e) {
+            System.out.println("error");
         }
-        catch (Exception e) {
-        System.out.println("error");
-        }
-    }    
-    
+    }
+
     @WebMethod()
     public void deleteBook(String booktitle) {
         try {
-        Books books = getBookApp().getBooks();
-        Book book = books.getBookByTitle(booktitle);
-        books.removeBook(book);
-        getBookApp().updateXML(books, "WEB-INF/books.xml");
-        getBookApp().saveBooks();        
+            Books books = getBookApp().getBooks();
+            Book book = books.getBookByTitle(booktitle);
+            books.removeBook(book);
+            getBookApp().updateXML(books, "WEB-INF/books.xml");
+            getBookApp().saveBooks();
+        } catch (Exception e) {
+            System.out.println("error");
         }
-        catch (Exception e) {
-        System.out.println("error");
-        }        
-    }    
-
+    }
 
 }
--- src/java/uts/wsd/soap/client/BookSOAPClient.java
+++ src/java/uts/wsd/soap/client/BookSOAPClient.java
@@ -8,115 +8,120 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author sungrinrhee
  */
-
 public class BookSOAPClient {
 
- public static void main(String[] args) {    
-  BookApp locator = new BookApp();
-  BookSOAP bookSOAP = locator.getBookSOAPPort();
-  Scanner scanner = new Scanner(System.in);
-  String select;     
-  User user = null;
-  String username;
-  String email;
-  do {
-    if (user != null) {
-    System.out.println("Welcome to Bookstore"); 
-    System.out.println("Select an option from below"); 
-    System.out.println("==========================="); 
-    System.out.println("v. View Books"); 
-    System.out.println("l. List a Book"); 
-    System.out.println("d. Delete a Book");
-    System.out.println("o. Logout");
-    System.out.println("x. Exit");
-    select = scanner.nextLine();   
-    }
-    else {
-    System.out.println("Welcome to Bookstore"); 
-    System.out.println("Select an option from below"); 
-    System.out.println("==========================="); 
-    System.out.println("v.View Books"); 
-    System.out.println("i.Login"); 
-    System.out.println("x. Exit");
-    select = scanner.nextLine();
-    }
-    switch (select) {   
-        case "v": 
-            List<Book> books = bookSOAP.fetchBooks().getBook();
-            for (Book book : books) {
-            System.out.println("Book Title: " + book.getBooktitle());  
-            System.out.println("Book Author: " + book.getAuthor());  
-            System.out.println("Book Category: " + book.getCategory() + "\n");
-            }
-            break;
-            
-        case "l": 
-            if(user!=null) {            
-            Book book = new Book();
-            System.out.println("Enter the details of your book.");  
-            System.out.println("Book title :");    
-            book.setBooktitle(scanner.nextLine());
-            System.out.println("Book author :");    
-            book.setAuthor(scanner.nextLine());
-            System.out.println("Book category :");    
-            book.setCategory(scanner.nextLine());
-            System.out.println("Book condition :");    
-            book.setCondition(scanner.nextLine());
-            System.out.println("Book isbn :");    
-            book.setIsbn(scanner.nextLine());
-            System.out.println("Book publishYear :");    
-            book.setPublishYear(scanner.nextInt());
-            scanner.nextLine();
-            System.out.println("Book publisher :");    
-            book.setPublisher(scanner.nextLine());
-            book.setUsername(user.getName());
-            System.out.println("Book abstruct :");    
-            book.setAbstract(scanner.nextLine());
-            bookSOAP.fetchBooks().getBook().add(book);
-            }
-            break;
-        case "d":
-            if(user!=null) {            
-            System.out.println("Enter the booktitle you want to delete"); 
-            String title = scanner.nextLine();
-            bookSOAP.deleteBook(title);
-            }
-            break;
-        case "i":
-            System.out.print("Enter email address: ");      
-            email = scanner.nextLine();
-            System.out.print("Enter password: ");      
-            String password = scanner.nextLine();   // password only stored temporarily. 
-            user = bookSOAP.userLogin(email, password);
-            if(user!=null) {
-                username = user.getName();
-                System.out.println("Hi, " + username); 
-                break;
-            } 
+    public static void main(String[] args) {
+        BookApp locator = new BookApp();
+        BookSOAP bookSOAP = locator.getBookSOAPPort();
+        Scanner scanner = new Scanner(System.in);
+        String select;
+        User user = null;
+        String username;
+        String email;
+        do {
+            System.out.println("===========================");
+            System.out.println("Welcome to Bookstore");
+            System.out.println("Select an option from below");
+            System.out.println("===========================");
 
-            else {
-                System.out.println("Error: Invalid email or password!");
-                break;
-            } 
-        case "o":
-            System.out.println("Logged out successfully.");
-            user = null;
-            break;
-        case "x":
-            break;
-        default :
-            System.out.println("Invalid input, please try again. \n");
-            break;
-    }
-  }
-  while(!select.equals("x")); 
-    System.out.println("Goodbye.");
+            if (user != null) {
+                System.out.println("v. View Books");
+                System.out.println("l. List a Book");
+                System.out.println("d. Delete a Book");
+                System.out.println("o. Logout");
+                System.out.println("x. Exit");
+                System.out.println("");
+                System.out.print("Your choice: ");
+                select = scanner.nextLine();
+            } else {
+                System.out.println("v. View Books");
+                System.out.println("i. Login");
+                System.out.println("x. Exit");
+                System.out.println("");
+                System.out.print("Your choice: ");
+                select = scanner.nextLine();
+            }
+            switch (select) {
+                case "v":
+                    List<Book> books = bookSOAP.fetchBooks().getBook();
+                    System.out.println("List of books:");
+                    for (Book book : books) {
+                        System.out.println("Book Title: " + book.getBooktitle());
+                        System.out.println("Book Author: " + book.getAuthor());
+                        System.out.println("Book Category: " + book.getCategory() + "\n");
+                    }
+                    break;
 
+                case "l":
+                    if (user != null) {
+                        System.out.println("Enter the details of your book:");
+                        System.out.print("Book title: ");
+                        String title = scanner.nextLine();
+
+                        System.out.print("Book author: ");
+                        String author = scanner.nextLine();
+
+                        System.out.print("Book category: ");
+                        String category = scanner.nextLine();
+
+                        System.out.print("Book condition: ");
+                        String condition = scanner.nextLine();
+
+                        System.out.print("Book isbn: ");
+                        String isbn = scanner.nextLine();
+
+                        System.out.print("Book publishYear: ");
+                        int publishYear = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.print("Book publisher: ");
+                        String publisher = scanner.nextLine();
+
+                        System.out.print("Book abstract: ");
+                        String abst = scanner.nextLine();
+
+                        System.out.println("Book listed successfully");
+                        bookSOAP.addBook(title, author, category, condition, isbn, publishYear, publisher, user.getName(), abst);
+                    }
+                    break;
+                case "d":
+                    if (user != null) {
+                        System.out.println("Enter the booktitle you want to delete:");
+                        String title = scanner.nextLine();
+                        bookSOAP.deleteBook(title);
+                        System.out.println("Book is deleted successfully");
+                    }
+                    break;
+                case "i":
+                    System.out.print("Enter email address: ");
+                    email = scanner.nextLine();
+                    System.out.print("Enter password: ");
+                    String password = scanner.nextLine();   // password only stored temporarily.
+                    user = bookSOAP.userLogin(email, password);
+                    if (user != null) {
+                        username = user.getName();
+                        System.out.println("Hi, " + username);
+                        break;
+                    } else {
+                        System.out.println("Error: Invalid email or password!");
+                        break;
+                    }
+                case "o":
+                    System.out.println("Logged out successfully.");
+                    user = null;
+                    break;
+                case "x":
+                    break;
+                default:
+                    System.out.println("Invalid input, please try again. \n");
+                    break;
+            }
+        } while (!select.equals("x"));
+        System.out.println("Goodbye.");
+
+    }
 }
-}
-
